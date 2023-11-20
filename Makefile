3b5dd8cf8f1a0211b93ff8d6c786879cdfaa91b3
Raunaq Singh <raunaqsingh2020@gmail.com>
2023-11-20 16:18:50 -0500

    sfml install

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: main
 
 main: main.cpp
-	g++ -g -Wall -std=c++20 main.cpp -o main
+	g++ -g -Wall -std=c++20 main.cpp -I/opt/homebrew/Cellar/sfml/2.6.1/include -o main -L/opt/homebrew/Cellar/sfml/2.6.1/lib -lsfml-graphics -lsfml-window -lsfml-system
 
 run: main
 	./main
--- main.cpp
+++ main.cpp
@@ -4,9 +4,29 @@
 
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
 #include "algorithms.hpp"
 
 int main()
 {
-    std::cout << "TO-DO" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
